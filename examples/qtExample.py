75e203a2ba19b7b13c4957f68de1b4b95e6e906e
Mosleh Uddin <captainwonka@MoeMacPro.local>
2018-01-17 19:18:03 -0500

    Added examples folder with basic QT(5) example.

---
+++ examples/qtExample.py
@@ -0,0 +1,18 @@
+# QT Example
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MyWidget(QtWidgets.QPushButton):
+  def __init__(self):
+    super(MyWidget,self).__init__()
+    self.setText("Hello World!!")
+    self.show()
+
+def main():
+  app = QtWidgets.QApplication(sys.argv)
+  myWidget = MyWidget()
+  sys.exit(app.exec_())
+
+if __name__ == '__main__':
+  main()
